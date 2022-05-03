e860b5a938e550c9823e10e087c43f882be8a76a
Marcelo MVV. Valenzuela V <mvalenzuela@incomerltda.cl>
2022-05-03 14:16:01 -0400

    actualizacion 03/05/2022

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,56 +1,55 @@
 const { format, transports } = require('winston');
 const winston = require('winston');
-
-/** Se exporta el metodo que crea los logs. Donde podemos configurar que queremos 
- * que nos muestre dicho log que se creara en una carpeta definida.
- */
+require('winston-daily-rotate-file');
 
 
-const logCorrespondencia = winston.createLogger({
-    transports: [
-        
-        new transports.File({
-            format: format.combine(
-                format.simple(),
-                format.timestamp({
-                    format: 'DD-MM-YYYY  HH:mm:ss A', // 25-01-2022 // 13:23:10 PM
-                  }),
-                format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
-            ),
-            maxsize: 5120000,
-            maxFiles: 7,
-            filename: `${__dirname}/../logs/logCorrespondencia.log`
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+    filename: `${__dirname}/../logs/logCorrespondencia %DATE%.log`,
+    datePattern: 'DD-MM-YYYY',
+    zippedArchive: true,
+    maxSize: '5m',
+    maxFiles: '30d',
+    format: format.combine(
+        format.simple(),
+        format.timestamp({
+    format: 'DD-MM-YYYY  HH:mm:ss A', // 25-01-2022 // 13:23:10 PM
+    }),
+    format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
+    )});
+
+const fileRotateTransportAdmin = new winston.transports.DailyRotateFile({
+        filename: `${__dirname}/../logs/logAdministracion %DATE%.log`,
+        datePattern: 'DD-MM-YYYY',
+        zippedArchive: true,
+        maxSize: '5m',
+        maxFiles: '30d',
+        format: format.combine(
+            format.simple(),
+            format.timestamp({
+        format: 'DD-MM-YYYY  HH:mm:ss A', // 25-01-2022 // 13:23:10 PM
         }),
-        new transports.Console({
-            level: 'debug',
-            
-        })
-    ]
-  });
+        format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
+        )});
+    
+fileRotateTransport.on('rotate', (oldFilename, newFilename) => {
+        
+});
+    
+const logCorrespondencia = winston.createLogger({                
+        transports: [
+            fileRotateTransport
+        ]
+});
 
 
 
 const logLogin = winston.createLogger({
     transports: [
-        
-        new transports.File({
-            format: format.combine(
-                format.simple(),
-                format.timestamp({
-                    format: 'DD-MM-YYYY  HH:mm:ss A', // 25-01-2022 // 13:23:10 PM
-                  }),
-                format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
-            ),
-            maxsize: 5120000,
-            maxFiles: 7,
-            filename: `${__dirname}/../logs/logAdministracion.log`
-        }),
-        new transports.Console({
-            level: 'debug',
-            
-        })
+        fileRotateTransportAdmin
     ]
-})
+}) 
+
+
 
 module.exports = {
     logLogin,
